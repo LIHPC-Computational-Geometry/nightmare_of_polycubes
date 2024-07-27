41a177db6321ad799a2ed26b916f02b51c485851
Sébastien Mestrallet <sebastien.mestrallet@hey.com>
2024-07-27 12:20:01 +0200

    update `in-volume_twist`

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,17 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [Unreleased]
+
+### Added
+
+- A triangle mesh (.obj), the naive labeling (.txt) and a glTF model are included for all models
+
+### Changed
+
+- Thumbnails now showcase the polycube labeling
+- `in-volume_twist` is more parametric and reoriented (to be seen from +Z)
+
 ## [1.0.0] - 2023-09-08
 
 ### Added
--- in-volume_twist/SALOME_Shaper.py
+++ in-volume_twist/SALOME_Shaper.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 ###
-### This file is based on the automatically generated one by SALOME v9.9.0 with dump python functionality
+### This file is based on the automatically generated one by SALOME v9.12.0 with dump python functionality
 ###
 
 import sys
@@ -25,33 +25,44 @@ partSet = model.moduleDocument()
 ### Create Part
 Part_1 = model.addPart(partSet)
 Part_1_doc = Part_1.document()
-model.addParameter(Part_1_doc, "L", '8*pi')
-model.addParameter(Part_1_doc, "margin", '3')
-model.addParameter(Part_1_doc, "l", '11')
+model.addParameter(Part_1_doc, "inner_margin", '2')
+model.addParameter(Part_1_doc, "outer_margin", '1')
+model.addParameter(Part_1_doc, "holes_width", '1')
+model.addParameter(Part_1_doc, "grooves_size", '0.5')
+model.addParameter(Part_1_doc, "box_length", '25.1327')
+model.addParameter(Part_1_doc, "box_width", '3*holes_width+2*inner_margin+2*outer_margin')
 
 ### Create Point
-Point_2 = model.addPoint(Part_1_doc, "0", "l/2-0.5", "l/2-0.5")
+Point_2 = model.addPoint(Part_1_doc, "0", "2*holes_width+inner_margin+outer_margin", "3*holes_width+2*inner_margin+outer_margin")
 
 ### Create Point
-Point_3 = model.addPoint(Part_1_doc, "0", "l/2-0.5", "l/2+0.5")
+Point_3 = model.addPoint(Part_1_doc, "0", "holes_width+inner_margin+outer_margin", "3*holes_width+2*inner_margin+outer_margin")
 
 ### Create Point
-Point_4 = model.addPoint(Part_1_doc, "0", "l/2+0.5", "l/2+0.5")
+Point_4 = model.addPoint(Part_1_doc, "0", "holes_width+inner_margin+outer_margin", "2*holes_width+2*inner_margin+outer_margin")
 
 ### Create Point
-Point_5 = model.addPoint(Part_1_doc, "0", "l/2+0.5", "l/2-0.5")
+Point_5 = model.addPoint(Part_1_doc, "0", "2*holes_width+inner_margin+outer_margin", "2*holes_width+2*inner_margin+outer_margin")
 
 ### Create Edge
-Edge_1 = model.addEdge(Part_1_doc, model.selection("VERTEX", "Point_3"), model.selection("VERTEX", "Point_2"))
+Edge_1 = model.addEdge(Part_1_doc, model.selection("VERTEX", "Point_1"), model.selection("VERTEX", "Point_2"))
+Edge_1.setName("Point_1_to_Point_2")
+Edge_1.result().setName("Edge_1_1")
 
 ### Create Edge
-Edge_2 = model.addEdge(Part_1_doc, model.selection("VERTEX", "Point_2"), model.selection("VERTEX", "Point_1"))
+Edge_2 = model.addEdge(Part_1_doc, model.selection("VERTEX", "Point_2"), model.selection("VERTEX", "Point_3"))
+Edge_2.setName("Point_2_to_Point_3")
+Edge_2.result().setName("Edge_2_1")
 
 ### Create Edge
-Edge_3 = model.addEdge(Part_1_doc, model.selection("VERTEX", "Point_1"), model.selection("VERTEX", "Point_4"))
+Edge_3 = model.addEdge(Part_1_doc, model.selection("VERTEX", "Point_3"), model.selection("VERTEX", "Point_4"))
+Edge_3.setName("Point_3_to_Point_4")
+Edge_3.result().setName("Edge_3_1")
 
 ### Create Edge
-Edge_4 = model.addEdge(Part_1_doc, model.selection("VERTEX", "Edge_3_1/Modified_Vertex&Point_4/Point_4"), model.selection("VERTEX", "Edge_1_1/Modified_Vertex&Point_3/Point_3"))
+Edge_4 = model.addEdge(Part_1_doc, model.selection("VERTEX", "Point_4"), model.selection("VERTEX", "Point_1"))
+Edge_4.setName("Point_4_to_Point_1")
+Edge_4.result().setName("Edge_4_1")
 
 ### Create Face
 Face_1_objects = [model.selection("EDGE", "Edge_1_1"),
@@ -59,138 +70,131 @@ Face_1_objects = [model.selection("EDGE", "Edge_1_1"),
                   model.selection("EDGE", "Edge_3_1"),
                   model.selection("EDGE", "Edge_4_1")]
 Face_1 = model.addFace(Part_1_doc, Face_1_objects)
-
-### Create Extrusion
-Extrusion_1 = model.addExtrusion(Part_1_doc, [model.selection("FACE", "Face_1_1")], model.selection(), model.selection(), 0, model.selection(), "L", "Faces|Wires")
+Face_1.result().setName("Face_1_1")
 
 ### Create Point
-Point_6 = model.addPoint(Part_1_doc, "0", "l/2-1.5-margin", "l/2-0.5")
-
-### Create Point
-Point_7 = model.addPoint(Part_1_doc, "0", "l/2-1.5-margin", "l/2+0.5")
-
-### Create Point
-Point_8 = model.addPoint(Part_1_doc, "0", "l/2-0.5-margin", "l/2+0.5")
-
-### Create Point
-Point_9 = model.addPoint(Part_1_doc, "0", "l/2-0.5-margin", "l/2-0.5")
-
-### Create Edge
-Edge_5 = model.addEdge(Part_1_doc, model.selection("VERTEX", "Point_7"), model.selection("VERTEX", "Point_6"))
-
-### Create Edge
-Edge_6 = model.addEdge(Part_1_doc, model.selection("VERTEX", "Point_6"), model.selection("VERTEX", "Point_5"))
-
-### Create Edge
-Edge_7 = model.addEdge(Part_1_doc, model.selection("VERTEX", "Point_5"), model.selection("VERTEX", "Point_8"))
-
-### Create Edge
-Edge_8 = model.addEdge(Part_1_doc, model.selection("VERTEX", "Edge_7_1/Modified_Vertex&Point_8/Point_8"), model.selection("VERTEX", "Edge_5_1/Modified_Vertex&Point_7/Point_7"))
-
-### Create Face
-Face_2_objects = [model.selection("EDGE", "Edge_5_1"),
-                  model.selection("EDGE", "Edge_6_1"),
-                  model.selection("EDGE", "Edge_7_1"),
-                  model.selection("EDGE", "Edge_8_1")]
-Face_2 = model.addFace(Part_1_doc, Face_2_objects)
-
-### Create Point
-Point_10 = model.addPoint(Part_1_doc, "0", "l/2-1-margin", "l/2")
+Point_6 = model.addPoint(Part_1_doc, "0", "3/2*holes_width+inner_margin+outer_margin", "5/2*holes_width+2*inner_margin+outer_margin")
+Point_6.setName("Face_1_middle")
+Point_6.result().setName("Point_5")
 
 ### Create Interpolation
-Interpolation_1 = model.addInterpolation(Part_1_doc, "t", "6*sin(t*0.125)+L/2-margin-1", "-3*sin(t*0.25)+(L/2)", 0, 25.13274122871834, 100)
-
-### Create Point
-Point_11 = model.addPoint(Part_1_doc, "0", "l/2-2-margin", "l/2")
-Point_11.setName("Point_11")
-Point_11.result().setName("Point_11")
-
-### Create Axis
-Axis_4 = model.addAxis(Part_1_doc, model.selection("VERTEX", "Point_9"), model.selection("VERTEX", "Point_11"))
-Axis_4.setName("Axis_2")
-Axis_4.result().setName("Axis_2")
+Interpolation_1 = model.addInterpolation(Part_1_doc, "t", "-1.5*inner_margin*sin(t*0.25)", "-3*inner_margin*sin(t*0.125)", 0, 25.1327, 100)
 
 ### Create Pipe
-Pipe_1 = model.addPipe(Part_1_doc, [model.selection("FACE", "Face_2_1")], model.selection("EDGE", "Interpolation_1_1"), model.selection("EDGE", "Axis_2"))
+Pipe_1 = model.addPipe(Part_1_doc, [model.selection("FACE", "Face_1_1")], model.selection("EDGE", "Interpolation_1_1"), model.selection("EDGE", "PartSet/OZ"))
 
 ### Create Box
-Box_1 = model.addBox(Part_1_doc, "L", "l", "l")
-
-### Create Cut
-Cut_1 = model.addCut(Part_1_doc, [model.selection("COMPOUND", "all-in-Box_1")], [model.selection("COMPOUND", "all-in-Extrusion_1")], keepSubResults = True)
-
-### Create Cut
-Cut_2 = model.addCut(Part_1_doc, [model.selection("COMPOUND", "all-in-Cut_1")], [model.selection("COMPOUND", "all-in-Pipe_1")], keepSubResults = True)
+Box_1 = model.addBox(Part_1_doc, "box_length", "box_width", "box_width")
 
 ### Create Sketch
-Sketch_1 = model.addSketch(Part_1_doc, model.selection("FACE", "Box_1_1/Top"))
+Sketch_1 = model.addSketch(Part_1_doc, model.selection("FACE", "Box_1_1/Back"))
 
 ### Create SketchLine
-SketchLine_1 = Sketch_1.addLine(0, 4, 0.5, 4)
-
-### Create SketchProjection
-SketchProjection_1 = Sketch_1.addProjection(model.selection("EDGE", "[Cut_2_1/Modified_Face&Box_1_1/Back][Box_1_1/Top]"), False)
-SketchLine_2 = SketchProjection_1.createdFeature()
-Sketch_1.setCoincident(SketchLine_1.startPoint(), SketchLine_2.result())
+SketchLine_1 = Sketch_1.addLine(4, -5, 5, -5)
 
 ### Create SketchLine
-SketchLine_3 = Sketch_1.addLine(0.5, 4, 0.5, 3.5)
-Sketch_1.setCoincident(SketchLine_1.endPoint(), SketchLine_3.startPoint())
-Sketch_1.setVertical(SketchLine_3.result())
+SketchLine_2 = Sketch_1.addLine(5, -5, 5, -4)
 
 ### Create SketchLine
-SketchLine_4 = Sketch_1.addLine(0.5, 3.5, 0, 3.5)
+SketchLine_3 = Sketch_1.addLine(5, -4, 4, -4)
+
+### Create SketchLine
+SketchLine_4 = Sketch_1.addLine(4, -4, 4, -5)
+Sketch_1.setCoincident(SketchLine_4.endPoint(), SketchLine_1.startPoint())
+Sketch_1.setCoincident(SketchLine_1.endPoint(), SketchLine_2.startPoint())
+Sketch_1.setCoincident(SketchLine_2.endPoint(), SketchLine_3.startPoint())
 Sketch_1.setCoincident(SketchLine_3.endPoint(), SketchLine_4.startPoint())
-Sketch_1.setCoincident(SketchLine_4.endPoint(), SketchLine_2.result())
-
-### Create SketchLine
-SketchLine_5 = Sketch_1.addLine(0, 4, 0, 3.5)
-Sketch_1.setCoincident(SketchLine_1.startPoint(), SketchLine_5.startPoint())
-Sketch_1.setCoincident(SketchLine_4.endPoint(), SketchLine_5.endPoint())
-Sketch_1.setHorizontal(SketchLine_1.result())
-Sketch_1.setHorizontal(SketchLine_4.result())
-Sketch_1.setDistance(SketchLine_4.endPoint(), SketchAPI_Line(SketchLine_2).startPoint(), 3.5, True)
-Sketch_1.setDistance(SketchLine_4.result(), SketchLine_3.startPoint(), 0.5, True)
-Sketch_1.setDistance(SketchLine_5.startPoint(), SketchLine_3.startPoint(), 0.5, True)
-model.do()
-
-### Create ExtrusionCut
-ExtrusionCut_1 = model.addExtrusionCut(Part_1_doc, [model.selection("FACE", "Sketch_1/Face-SketchLine_5f-SketchLine_4r-SketchLine_3r-SketchLine_1r")], model.selection(), 0, 11, [model.selection("SOLID", "Cut_2_1")])
-
-### Create Sketch
-Sketch_2 = model.addSketch(Part_1_doc, model.selection("FACE", "ExtrusionCut_1_1/Modified_Face&Box_1_1/Top"))
-
-### Create SketchLine
-SketchLine_6 = Sketch_2.addLine(25.13274122871834, 4, 24.63274122871834, 4)
+Sketch_1.setPerpendicular(SketchLine_1.result(), SketchLine_2.result())
+Sketch_1.setPerpendicular(SketchLine_2.result(), SketchLine_3.result())
+Sketch_1.setPerpendicular(SketchLine_3.result(), SketchLine_4.result())
+Sketch_1.setHorizontalDistance(SketchLine_2.startPoint(), SketchLine_4.endPoint(), "holes_width")
+Sketch_1.setVerticalDistance(SketchLine_2.startPoint(), SketchLine_3.startPoint(), "holes_width")
 
 ### Create SketchProjection
-SketchProjection_2 = Sketch_2.addProjection(model.selection("EDGE", "[ExtrusionCut_1_1/Modified_Face&Box_1_1/Top][Cut_2_1/Modified_Face&Box_1_1/Front]"), False)
-SketchLine_7 = SketchProjection_2.createdFeature()
-Sketch_2.setCoincident(SketchLine_6.startPoint(), SketchLine_7.result())
-Sketch_2.setHorizontal(SketchLine_6.result())
-
-### Create SketchLine
-SketchLine_8 = Sketch_2.addLine(24.63274122871834, 4, 24.63274122871834, 3.5)
-Sketch_2.setCoincident(SketchLine_6.endPoint(), SketchLine_8.startPoint())
-Sketch_2.setVertical(SketchLine_8.result())
-
-### Create SketchLine
-SketchLine_9 = Sketch_2.addLine(24.63274122871834, 3.5, 25.13274122871834, 3.5)
-Sketch_2.setCoincident(SketchLine_8.endPoint(), SketchLine_9.startPoint())
-Sketch_2.setCoincident(SketchLine_9.endPoint(), SketchLine_7.result())
-Sketch_2.setHorizontal(SketchLine_9.result())
-
-### Create SketchLine
-SketchLine_10 = Sketch_2.addLine(25.13274122871834, 4, 25.13274122871834, 3.5)
-Sketch_2.setCoincident(SketchLine_6.startPoint(), SketchLine_10.startPoint())
-Sketch_2.setCoincident(SketchLine_9.endPoint(), SketchLine_10.endPoint())
-Sketch_2.setDistance(SketchLine_9.endPoint(), SketchAPI_Line(SketchLine_7).startPoint(), 3.5, True)
-Sketch_2.setDistance(SketchLine_8.endPoint(), SketchLine_8.startPoint(), 0.5, True)
-Sketch_2.setDistance(SketchLine_8.startPoint(), SketchLine_10.startPoint(), 0.5, True)
+SketchProjection_1 = Sketch_1.addProjection(model.selection("VERTEX", "[Box_1_1/Back][Box_1_1/Left][Box_1_1/Bottom]"), False)
+SketchPoint_1 = SketchProjection_1.createdFeature()
+Sketch_1.setVerticalDistance(SketchLine_3.startPoint(), SketchAPI_Point(SketchPoint_1).coordinates(), "inner_margin+holes_width+outer_margin")
+Sketch_1.setHorizontal(SketchLine_1.result())
+Sketch_1.setHorizontalDistance(SketchLine_3.endPoint(), SketchAPI_Point(SketchPoint_1).coordinates(), "inner_margin+holes_width+outer_margin")
 model.do()
 
 ### Create ExtrusionCut
-ExtrusionCut_2 = model.addExtrusionCut(Part_1_doc, [model.selection("FACE", "Sketch_2/Face-SketchLine_6r-SketchLine_8f-SketchLine_9f-SketchLine_10r")], model.selection(), 0, 11, [model.selection("SOLID", "ExtrusionCut_1_1")])
+ExtrusionCut_1 = model.addExtrusionCut(Part_1_doc, [model.selection("FACE", "Sketch_1/Face-SketchLine_1r-SketchLine_2f-SketchLine_3f-SketchLine_4f")], model.selection(), 0, "box_length", [model.selection("SOLID", "Box_1_1")])
+
+### Create Cut
+Cut_1 = model.addCut(Part_1_doc, [model.selection("SOLID", "ExtrusionCut_1_1")], [model.selection("SOLID", "Pipe_1_1")], keepSubResults = True)
+
+### Create Sketch
+Sketch_2 = model.addSketch(Part_1_doc, model.selection("FACE", "Box_1_1/Left"))
+
+### Create SketchLine
+SketchLine_5 = Sketch_2.addLine(0.5, 6.25, 0, 6.25)
+
+### Create SketchProjection
+SketchProjection_2 = Sketch_2.addProjection(model.selection("EDGE", "[Cut_1_1/Modified_Face&Box_1_1/Back][Box_1_1/Left]"), False)
+SketchLine_6 = SketchProjection_2.createdFeature()
+Sketch_2.setCoincident(SketchLine_5.endPoint(), SketchLine_6.result())
+
+### Create SketchLine
+SketchLine_7 = Sketch_2.addLine(0, 6.25, 0, 5.75)
+
+### Create SketchLine
+SketchLine_8 = Sketch_2.addLine(0, 5.75, 0.5, 5.75)
+
+### Create SketchLine
+SketchLine_9 = Sketch_2.addLine(0.5, 5.75, 0.5, 6.25)
+Sketch_2.setCoincident(SketchLine_9.endPoint(), SketchLine_5.startPoint())
+Sketch_2.setCoincident(SketchLine_5.endPoint(), SketchLine_7.startPoint())
+Sketch_2.setCoincident(SketchLine_7.endPoint(), SketchLine_8.startPoint())
+Sketch_2.setCoincident(SketchLine_8.endPoint(), SketchLine_9.startPoint())
+Sketch_2.setPerpendicular(SketchLine_5.result(), SketchLine_7.result())
+Sketch_2.setPerpendicular(SketchLine_7.result(), SketchLine_8.result())
+Sketch_2.setPerpendicular(SketchLine_8.result(), SketchLine_9.result())
+
+### Create SketchLine
+SketchLine_10 = Sketch_2.addLine(24.6327, 6.25, 25.1327, 6.25)
+
+### Create SketchProjection
+SketchProjection_3 = Sketch_2.addProjection(model.selection("EDGE", "[Box_1_1/Left][Cut_1_1/Modified_Face&Box_1_1/Front]"), False)
+SketchLine_11 = SketchProjection_3.createdFeature()
+Sketch_2.setCoincident(SketchLine_10.endPoint(), SketchLine_11.result())
+
+### Create SketchLine
+SketchLine_12 = Sketch_2.addLine(25.1327, 6.25, 25.1327, 5.75)
+
+### Create SketchLine
+SketchLine_13 = Sketch_2.addLine(25.1327, 5.75, 24.6327, 5.75)
+
+### Create SketchLine
+SketchLine_14 = Sketch_2.addLine(24.6327, 5.75, 24.6327, 6.25)
+Sketch_2.setCoincident(SketchLine_14.endPoint(), SketchLine_10.startPoint())
+Sketch_2.setCoincident(SketchLine_10.endPoint(), SketchLine_12.startPoint())
+Sketch_2.setCoincident(SketchLine_12.endPoint(), SketchLine_13.startPoint())
+Sketch_2.setCoincident(SketchLine_13.endPoint(), SketchLine_14.startPoint())
+Sketch_2.setPerpendicular(SketchLine_10.result(), SketchLine_12.result())
+Sketch_2.setPerpendicular(SketchLine_12.result(), SketchLine_13.result())
+Sketch_2.setPerpendicular(SketchLine_13.result(), SketchLine_14.result())
+Sketch_2.setVertical(SketchLine_7.result())
+Sketch_2.setVertical(SketchLine_12.result())
+Sketch_2.setVerticalDistance(SketchLine_9.endPoint(), SketchLine_14.endPoint(), 0)
+Sketch_2.setVerticalDistance(SketchLine_8.endPoint(), SketchLine_13.endPoint(), 0)
+Sketch_2.setVerticalDistance(SketchLine_9.endPoint(), SketchLine_8.endPoint(), "grooves_size")
+
+### Create SketchPoint
+SketchPoint_2 = Sketch_2.addPoint(0.5000000000000006, 6)
+SketchPoint_2.setAuxiliary(True)
+Sketch_2.setCoincident(SketchPoint_2.coordinates(), SketchLine_9.result())
+Sketch_2.setMiddlePoint(SketchPoint_2.coordinates(), SketchLine_9.result())
+Sketch_2.setVerticalDistance(SketchPoint_2.coordinates(), SketchAPI_Line(SketchLine_6).endPoint(), "outer_margin+holes_width+inner_margin/2")
+Sketch_2.setHorizontalDistance(SketchLine_7.endPoint(), SketchLine_8.endPoint(), "grooves_size")
+Sketch_2.setHorizontalDistance(SketchLine_14.startPoint(), SketchLine_12.endPoint(), "grooves_size")
+model.do()
+
+### Create ExtrusionCut
+ExtrusionCut_2 = model.addExtrusionCut(Part_1_doc, [model.selection("FACE", "Sketch_2/Face-SketchLine_5r-SketchLine_7f-SketchLine_8f-SketchLine_9f")], model.selection(), 0, "box_width", [model.selection("SOLID", "Cut_1_1")])
+
+### Create ExtrusionCut
+ExtrusionCut_3 = model.addExtrusionCut(Part_1_doc, [model.selection("WIRE", "Sketch_2/Face-SketchLine_14r-SketchLine_13r-SketchLine_12r-SketchLine_10r_wire")], model.selection(), 0, "box_width", [model.selection("SOLID", "ExtrusionCut_2_1")])
 
 model.end()
 
@@ -200,7 +204,7 @@ model.end()
 
 model.publishToShaperStudy()
 import SHAPERSTUDY
-ExtrusionCut_2_1, = SHAPERSTUDY.shape(model.featureStringId(ExtrusionCut_2))
+ExtrusionCut_3_1, = SHAPERSTUDY.shape(model.featureStringId(ExtrusionCut_3))
 
 if salome.sg.hasDesktop():
   salome.sg.updateObjBrowser()
